4830a2890cdf4e19195e7b8cbe1b857eac47ee61
Andy <gmq0311@163.com>
2026-04-19 17:03:48 +0800

    0419

---
+++ src/Snake.java
@@ -0,0 +1,172 @@
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+public class Snake extends GameEngine {
+    public static void main(String[] args) {
+        createGame(new Snake());
+    }
+
+
+    class Point {
+        private int x, y;
+        public Point(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        void setPoint(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+        void take(){
+            grid[x][y] = true;
+        }
+        void release(){
+            grid[x][y] = false;
+        }
+        int getX(){
+            return x;
+        }
+        int getY(){
+            return y;
+        }
+        //boolean isHead;
+    }
+
+    boolean gameOver;
+    boolean[][] grid = new boolean[50][50];
+    boolean up, down, left, right;
+    int direction;  //up,down,left,right  0,1,2,3
+    ArrayList<Point> body = new ArrayList<>();
+    boolean appleExist, eatApple;
+    int applePointX, applePointY;
+
+    Image apple = loadImage("resources/apple.png");
+    Image dot = loadImage("resources/dot.png");
+    Image head = loadImage("resources/head.png");
+
+    public void randomApple(){
+        do{
+            applePointX = rand(50);
+            applePointY = rand(50);
+        }while (grid[applePointX][applePointY]);
+        appleExist = true;
+
+    }
+
+    public void init(){
+        gameOver = false;
+        body.add(new Point(25, 25));
+        body.add(new Point(25, 26));
+        body.add(new Point(25, 27));
+        grid[25][25]=true;
+        grid[25][26]=true;
+        grid[25][27]=true;
+        eatApple = false;
+        appleExist = false;
+        randomApple();
+        direction = 0;
+
+
+
+    }
+    public void drawMap(){
+        changeColor(white);
+        for(int i=0; i<= 50; i++){
+            drawLine(0,i*10,500,i*10,0.05);
+            drawLine(i*10,0,i*10,500,0.05);
+        }
+    }
+
+
+
+    public void upDataSnake(){
+        Point temp = new Point(body.getFirst().getX(),body.getFirst().getY());
+        switch (direction){
+            case 0://up
+                temp.setPoint(temp.getX(),temp.getY()-1);
+                break;
+            case 1://down
+                temp.setPoint(temp.getX(),temp.getY()+1);
+                break;
+            case 2://left
+                temp.setPoint(temp.getX()-1,temp.getY());
+                break;
+            case 3://right
+                temp.setPoint(temp.getX()+1,temp.getY());
+                break;
+        }
+        if (temp.getX() < 0 || temp.getX() >= 50 || temp.getY() < 0 || temp.getY() >= 50) {
+            gameOver = true;
+            return;
+        }
+        if(grid[temp.getX()][temp.getY()]){
+            gameOver = true;
+            return;
+        }
+        body.addFirst(temp);
+        temp.take();
+
+        if(!(eatApple && body.size() < 22)){
+            body.removeLast();
+            body.getLast().release();
+        }
+
+
+    }
+
+    public void drawSnake(){
+        drawImage(head, body.getFirst().getX() * 10, body.getFirst().getY() * 10, 9, 9);
+
+        for (int i = 1; i < body.size(); i++) {
+            Point point = body.get(i);
+            drawImage(dot, point.getX() * 10, point.getY() * 10, 9, 9);
+        }
+    }
+
+    public void drawApple(){
+        drawImage(apple,applePointX*10+0.5,applePointY*10+0.5,9,9);
+    }
+
+    public void keyPressed(KeyEvent e){
+        if (e.getKeyCode() == KeyEvent.VK_LEFT && direction!=3){
+            direction = 2;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT && direction!=2){
+            direction = 3;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP && direction!=1){
+            direction = 0;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN && direction!=0){
+            direction = 1;
+        }
+    }
+
+    double timer = 0;
+    double moveDelay = 0.2;
+    @Override
+    public void update(double dt) {
+        timer += dt;
+        if (timer >= moveDelay) {
+            upDataSnake();
+            timer = 0;
+        }
+    }
+
+    @Override
+    public void paintComponent() {
+        changeBackgroundColor(black);
+        clearBackground(500,500);
+        drawMap();
+        if (!gameOver){
+            drawSnake();
+            drawApple();
+        } else {
+            changeColor(white);
+            drawText(85, 250, "GAME OVER!", "Arial", 50);
+        }
+
+    }
+}
